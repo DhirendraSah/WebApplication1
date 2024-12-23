d3c3ea8206849ccf2adf0a484b7e57fe61971e20
DhirendraSah <sahdhirendra93@gmail.com>
2024-12-23 21:02:08 +0530

    final change

--- WebForm1.aspx
+++ WebForm1.aspx
@@ -9,8 +9,33 @@
 <body>
     <form id="form1" runat="server">
         <div>
-            hello world
-            how are you all
+            <frameset >
+                <label>Simple calculator</label><br />
+                <br />
+                <label>Num1:</label>
+                <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                <br />
+                <label>Num2:</label>
+                <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                <br />
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="plus" runat="server" Text="+" Width="53px" OnClick="Button1_Click1" />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="minus" runat="server" Text="-" Width="41px" OnClick="minus_Click" />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="mul" runat="server" Text="*" Width="43px" OnClick="mul_Click" />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="div" runat="server" Text="/" Width="47px" OnClick="div_Click" />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="mod" runat="server" Text="%" Width="49px" OnClick="mod_Click" />
+                <br />
+                <asp:Label ID="Output" runat="server" Text="Result:"></asp:Label>
+
+            </frameset>
+            
+        	
+            
         </div>
     </form>
 </body>
--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -8,10 +8,68 @@ using System.Web.UI.WebControls;
 namespace WebApplication1
 {
 	public partial class WebForm1 : System.Web.UI.Page
-	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
 
+	{
+
+		protected void Button1_Click1(object sender, EventArgs e)
+		{
+			if (double.TryParse(TextBox1.Text, out double num1) && double.TryParse(TextBox2.Text, out double num2))
+			{
+				Output.Text = "Result: " + (num1 + num2);
+			}
+			else
+			{
+				Output.Text = "Please enter valid numbers.";
+			}
+
+		}
+
+		protected void minus_Click(object sender, EventArgs e)
+		{
+			if (double.TryParse(TextBox1.Text, out double num1) && double.TryParse(TextBox2.Text, out double num2))
+			{
+				Output.Text = "Result: " + (num1 - num2);
+			}
+			else
+			{
+				Output.Text = "Please enter valid numbers.";
+			}
+		}
+
+		protected void mul_Click(object sender, EventArgs e)
+		{
+			if (double.TryParse(TextBox1.Text, out double num1) && double.TryParse(TextBox2.Text, out double num2))
+			{
+				Output.Text = "Result: " + (num1 * num2);
+			}
+			else
+			{
+				Output.Text = "Please enter valid numbers.";
+			}
+		}
+
+		protected void div_Click(object sender, EventArgs e)
+		{
+			if (double.TryParse(TextBox1.Text, out double num1) && double.TryParse(TextBox2.Text, out double num2))
+			{
+				Output.Text = "Result: " + (num1 / num2);
+			}
+			else
+			{
+				Output.Text = "Please enter valid numbers.";
+			}
+		}
+
+		protected void mod_Click(object sender, EventArgs e)
+		{
+			if (double.TryParse(TextBox1.Text, out double num1) && double.TryParse(TextBox2.Text, out double num2))
+			{
+				Output.Text = "Result: " + (num1 % num2);
+			}
+			else
+			{
+				Output.Text = "Please enter valid numbers.";
+			}
 		}
 	}
 }
